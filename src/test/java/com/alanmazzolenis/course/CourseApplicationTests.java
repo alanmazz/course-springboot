05de013adcb53e9ad86d2665c39b2b02db7900fa
Alan Mazzolenis <alangalvaogg@gmail.com>
2022-03-18 17:38:31 -0300

    Project created

---
+++ src/test/java/com/alanmazzolenis/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alanmazzolenis.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
