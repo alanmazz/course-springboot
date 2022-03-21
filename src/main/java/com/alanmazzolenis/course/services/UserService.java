43b1ea957c7dc1ce0ff5365a55fe58182528291d
Alan Mazzolenis <alangalvaogg@gmail.com>
2022-03-21 10:49:39 -0300

    Service layer, component registration

---
+++ src/main/java/com/alanmazzolenis/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.alanmazzolenis.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alanmazzolenis.course.entities.User;
+import com.alanmazzolenis.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
